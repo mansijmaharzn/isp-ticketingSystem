2de9270e06d6094c400dec8b1a5c0c2c8b2b2654
Mansij Maharjan <71772378+mansijmaharzn@users.noreply.github.com>
2022-06-25 21:03:19 +0545

    add accessibility
    zoom in, out, reset zoom

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,4 @@
+// For Ticket Status Updates
 function closeTicket(ticketId) {
     fetch("/close-ticket", {
         method: "POST",
@@ -15,3 +16,31 @@ function solveTicket(ticketId) {
         window.location.href = "/admin-dashboard";
     });
 }
+
+// For zoom Accessibility
+let zoom = 1;
+let zoomStep = 0.1;
+const zoomText = document.getElementsByClassName("zoomtext");
+
+document.getElementById("zoomIn").addEventListener("click", function () {
+    zoom += zoomStep;
+    for (let i = 0; i < zoomText.length; i++) {
+        zoomText[i].style.transform = "scale(" + zoom + ")";
+    }
+});
+document.getElementById("zoomOut").addEventListener("click", function () {
+    if (zoom > zoomStep) {
+        zoom -= zoomStep;
+        for (let i = 0; i < zoomText.length; i++) {
+            zoomText[i].style.transform = "scale(" + zoom + ")";
+        }
+    }
+});
+document.getElementById("zoomReset").addEventListener("click", function () {
+    zoom = 1;
+    for (let i = 0; i < zoomText.length; i++) {
+        zoomText[i].style.transform = "scale(" + zoom + ")";
+    }
+})
+
+// zoomReset
